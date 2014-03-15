104ea8945a8e543fc211b941d2ffc7f4ab8ac37b
matrostik <matrostik@gmail.com>
2014-03-15 20:02:43 +0200

    Adde bootstrap theme (slate)

--- HelloJCE/App_Start/BundleConfig.cs
+++ HelloJCE/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace HelloJCE
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/slate.css",
                       "~/Content/site.css"));
         }
     }
