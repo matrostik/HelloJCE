f78cac095827936c0d3d8a0a099fc9d3679404e8
matrostik <matrostik@gmail.com>
2014-03-15 16:50:37 +0200

    Initial version

---
+++ HelloJCE/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HelloJCE.MvcApplication" Language="C#" %>
---
+++ HelloJCE/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelloJCE.Startup))]
+namespace HelloJCE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
