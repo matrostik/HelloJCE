68a1503d6bc69a2724626308f5c04cebea9d797f
matrostik <matrostik@gmail.com>
2014-03-15 23:10:44 +0200

    Movies controller + views + DB connection

--- HelloJCE/App_Start/RouteConfig.cs
+++ HelloJCE/App_Start/RouteConfig.cs
@@ -18,6 +18,10 @@ namespace HelloJCE
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            routes.MapRoute(
+            name: "Hello",
+            url: "{controller}/{action}/{name}/{id}"
+           );
         }
     }
 }
---
+++ HelloJCE/Controllers/ListController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloJCE.Controllers
+{
+    public class ListController : Controller
+    {
+        //
+        // GET: /List/
+        public ActionResult Index() 
+        {
+            return View();
+        } 
+ 
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+	}
+}
