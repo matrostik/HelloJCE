56dcd6e9b5440ab3449e5fb25a05470460698459
matrostik <matrostik@gmail.com>
2014-03-17 15:39:40 +0200

    image helper

---
+++ HelloJCE/Helpers/ImageHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+
+namespace HelloJCE.Helpers
+{
+    public class ImageHelper
+    {
+        public static string Image(this HtmlHelper helper, string url)
+        {
+            var writer = new HtmlTextWriter(new StringWriter());
+
+            writer.AddAttribute(HtmlTextWriterAttribute.Src, url);
+            writer.RenderBeginTag(HtmlTextWriterTag.Img);
+            writer.RenderEndTag();
+
+            return writer.InnerWriter.ToString();
+        }
+    }
+}
