2fa23fe67f8464eb3a6a1e22b724c47fa3d133a2
rosh <rosh@balink.net>
2014-03-26 09:44:03 +0200

    RegistrationConfirm link fixed

---
+++ HelloJCE/Helpers/AppSettings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloJCE.Helpers
+{
+    public static class AppSettings
+    {
+        public static string ClientSecret
+        {
+            get
+            {
+                return Setting<string>("ClientSecret");
+            }
+        }
+
+        private static T Setting<T>(string name)
+        {
+            string value = ConfigurationManager.AppSettings[name];
+
+            if (value == null)
+            {
+                throw new Exception(String.Format("Could not find setting '{0}',", name));
+            }
+
+            return (T)Convert.ChangeType(value, typeof(T), CultureInfo.InvariantCulture);
+        }
+    }
+}
