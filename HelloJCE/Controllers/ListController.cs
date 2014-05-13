d103d36edbf8ff6e53d3c45d2d6ed2c74eaff2cb
rosh <rosh@balink.net>
2014-05-13 13:42:41 +0300

    12

--- HelloJCE/Controllers/ListController.cs
+++ HelloJCE/Controllers/ListController.cs
@@ -218,7 +218,7 @@ namespace HelloJCE.Controllers
             catch (Exception s)
             {
                 //MessageBox.Show("Something went wrong. " + s.Message);
-                return "Failed!";
+                return s.Message;
             }
         }
 
